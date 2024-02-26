b2fee00788dc94f934a6e031c33595bc30461343
Bernardo Andrade <berdfandrade@gmail.com>
2024-02-26 16:28:31 -0300

    --

---
+++ javascriptImplementation.js
@@ -0,0 +1,37 @@
+let running = true;
+let ip = 0;
+let sp = -1;
+
+const stack = [];
+
+const InstructionSet = {
+  PSH: 0,
+  ADD: 1,
+  POP: 2,
+  HLT: 3,
+};
+
+const program = [
+  InstructionSet.PSH,
+  5,
+  InstructionSet.PSH,
+  6,
+  InstructionSet.ADD,
+  InstructionSet.POP,
+  InstructionSet.HLT,
+];
+
+function eval(instr) {
+  switch (instr) {
+    case InstructionSet.HLT: {
+      running = false;
+      console.log("done");
+      break;
+    }
+    case InstructionSet.PSH: {
+      sp++;
+      stack.push(program[++ip]);
+      break;
+    }
+  }
+}
